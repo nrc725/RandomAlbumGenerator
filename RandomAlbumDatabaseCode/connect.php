af301ccebf58cf8542bd0715e003030b3bb49f0a
Nick Currie <nrc725@yahoo.com>
2019-07-06 13:51:45 -0400

    Adds database code
    Adds php code used to get album information from mysql database

---
+++ RandomAlbumDatabaseCode/connect.php
@@ -0,0 +1,7 @@
+<?php
+$db_name = "albums";
+$mysql_username = "root";
+$mysql_password = "";
+$server_name = "localhost";
+$connect = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
+?>
